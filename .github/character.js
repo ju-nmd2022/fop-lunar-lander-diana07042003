9a3c47ba83320698771b74a790a252a190d494f4
Diana Man <madi22gc@student.ju.se>
2023-02-21 15:21:33 +0200

    update

---
+++ .github/character.js
@@ -0,0 +1,32 @@
+background(255, 255, 255);
+
+//Tomato
+
+push();
+fill(139, 0, 0);
+noStroke();
+ellipse(300, 280, 40);
+pop();
+
+push();
+fill(1, 50, 32);
+
+beginShape();
+ 
+ vertex(300, 265);
+ bezierVertex(290, 230, 290, 275, 300, 270);
+ bezierVertex(280, 270, 280, 280, 310, 275);
+ 
+endShape();
+
+pop();
+
+
+
+
+
+
+
+
+
+
