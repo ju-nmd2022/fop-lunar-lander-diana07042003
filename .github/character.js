f57ddc7e651d38b7474851c696b1a59b7d200ef0
Diana Man <madi22gc@student.ju.se>
2023-03-04 15:48:22 +0200

    Update character.js
    update, start screen

--- .github/character.js
+++ .github/character.js
@@ -1,82 +1,118 @@
-background(255, 255, 255);
+var mode;
 
-//Tomato
+function setup(){
+mode = 0;
+textSize(21);
+}
 
-push();
-fill(139, 0, 0);
-noStroke();
-ellipse(300, 280, 40);
-pop();
+function draw(){
+    clear();
+    if (mode==0){
+        text('Press enter to start', 20, 40);
 
-push();
-fill(1, 50, 32);
+        }
 
-beginShape();
- 
- vertex(300, 265);
- bezierVertex(290, 230, 290, 275, 300, 270);
- bezierVertex(280, 270, 280, 280, 310, 275);
- 
-endShape();
+    if (mode==1){
+        background(249, 232, 163);
 
-pop();
+        //Tomato
+        
+        push();
+        fill(139, 0, 0);
+        noStroke();
+        ellipse(300, 280, 40);
+        pop();
+        
+        push();
+        fill(1, 50, 32);
+        
+        beginShape();
+         
+         vertex(300, 265);
+         bezierVertex(290, 230, 290, 275, 300, 270);
+         bezierVertex(280, 270, 280, 280, 310, 275);
+         
+        endShape();
+        
+        pop();
+        
+        //Table
+        
+        fill(31,115,81);
+        noStroke();
+        rect(0, 620, 800, 700);
+        
+        
+        //Plate
+        
+        fill(175,167,55);
+        noStroke();
+        rect(265, 625, 230, 110);
+        
+        
+        push();
+        fill(255, 255, 255);
+        
+        stroke(202, 132, 62);
+        strokeWeight(15);
+        ellipse(380, 680, 170, 100);
+        pop();
+        
+        
+        push();
+        
+        fill(85, 47, 9);
+        rect(480, 660, 5, 70, 5);
+        ellipse(482, 660, 25, 30);
+        pop();
+        
+        push();
+        fill(175,167,55);
+        ellipse(482, 650, 20, 45);
+        pop();
+        
+        push();
+        fill(85, 47, 9);
+        rect(481, 653, 3, 70);
+        pop();
+        
+        
+        //Spaghetti
+        
+        push();
+        
+        pop();
+        
+        //Obstacles
+        
+        fill(139,69,19);
+        rect(400, 50, 400, 20);
+        rect(0, 100, 300, 20);
+        rect(0, 180, 400, 20);
+        rect(400, 280, 400, 20);
+        rect(0, 500, 250, 20);
+        rect(380, 470, 450, 20);
+        rect(0, 350, 200, 20);
+        rect(300, 400, 500, 20);
+    }
+}
 
-//Table
-
-fill(31,115,81);
-noStroke();
-rect(0, 620, 800, 700);
-
-
-//Plate
-
-fill(175,167,55);
-noStroke();
-rect(265, 625, 230, 110);
-
-
-push();
-fill(255, 255, 255);
-
-stroke(202, 132, 62);
-strokeWeight(15);
-ellipse(380, 680, 170, 100);
-pop();
-
-
-push();
-
-fill(85, 47, 9);
-rect(480, 660, 5, 70, 5);
-ellipse(482, 660, 25, 30);
-pop();
-
-push();
-fill(175,167,55);
-ellipse(482, 650, 20, 45);
-pop();
-
-push();
-fill(85, 47, 9);
-rect(481, 653, 3, 70);
-pop();
-
-
-//Spaghetti
+function keyPressed(){
+    if (keyCode===ENTER){
+        mode=1;
+    }
+}
 
 
 
-//Obstacles
 
-fill(139,69,19);
-rect(400, 50, 400, 20);
-rect(0, 100, 300, 20);
-rect(0, 180, 400, 20);
-rect(400, 280, 400, 20);
-rect(0, 500, 250, 20);
-rect(380, 470, 450, 20);
-rect(0, 350, 200, 20);
-rect(300, 400, 500, 20);
+
+
+
+
+
+
+
 
 
 
